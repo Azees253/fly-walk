385f11ac4cbba4020c3b344b01467b0cba922826
Azees253 <mohamedazees61@gmail.com>
2025-05-29 18:41:01 +0530

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
   const [CollectionItems, setCollectioItems] = useState([]);
   const [itemCount, setItemCount] = useState({});
 
-  const url = "http://localhost:4000";
+  const url = "https://fly-walk-backend.onrender.com";
 
   const loadCartData = async (token) => {
     const response = await axios.post(
