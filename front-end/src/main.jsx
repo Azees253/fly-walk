e75a9220bb58c99caba1a0a8f539af2d03c18fb3
Azees253 <mohamed61@gmail.com>
2025-06-03 19:25:25 +0530

    updates

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/fly-walk/">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
